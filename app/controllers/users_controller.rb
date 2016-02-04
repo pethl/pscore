65b3d7332f0dc4a2671c2dbbfb9c5a94512d90b1
pethl <pethicklisa@gmail.com>
2016-02-04 17:26:39 +0000

    enable admin to reset passwords

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,7 @@
 class UsersController < ApplicationController
 
   before_filter :signed_in_user, only: [:edit, :update, :destroy, ]
-  before_filter :correct_user,   only: [:edit, :update]
-  before_filter :admin_user,     only: :destroy
+  before_filter :admin_user,     only: [:edit, :update, :destroy]
   
   def fetch_user
       @user = User.find_by_id(params[:id])
